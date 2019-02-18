33512a27febee97a548d69f44b735f37da18250f
ippolite <gurrman@outlook.com>
2019-02-18 13:45:09 +0300

    Test commit

---
+++ open-load.C
@@ -0,0 +1,5 @@
+{
+	gSystem->Load("libDataTree.so");
+	TFile *rf = new TFile("~/Projects/cbm_12agev_20K.root","READ");
+//	DataTree->Draw("fTracks.fMomentum.Py():fTracks.fMomentum.Px()","","colz");
+}
