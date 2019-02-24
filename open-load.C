ffda1b69202ffb09e8e17eb43006cbe85291fe81
ippolite <gurrman@outlook.com>
2019-02-24 14:57:00 +0300

    Initial commit

--- open-load.C
+++ open-load.C
@@ -1,5 +1,4 @@
 {
 	gSystem->Load("libDataTree.so");
 	TFile *rf = new TFile("~/Projects/cbm_12agev_20K.root","READ");
-//	DataTree->Draw("fTracks.fMomentum.Py():fTracks.fMomentum.Px()","","colz");
 }
---
+++ open-write.C
@@ -0,0 +1,6 @@
+{
+	gSystem->Load("libDataTree.so");
+	TFile *rf = new TFile("~/Projects/cbm_12agev_20K.root","READ");
+//	DataTree->Draw("fTracks.fMomentum.Py():fTracks.fMomentum.Px()","","colz");
+//below this point a writer would be
+}
